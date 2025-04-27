65de61bd106de6b8e248ea24f7c5f998093ad5dd
zannatulferdous0162 <zannatulferdous0162@.com>
2025-04-27 17:03:01 +0600

    Update deployment scripts

---
+++ Migrations/20250427105948_zannat.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoreManagement_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class zannat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_LocationComponents_BinId",
+                table: "LocationComponents",
+                column: "BinId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LocationComponents_Bins_BinId",
+                table: "LocationComponents",
+                column: "BinId",
+                principalTable: "Bins",
+                principalColumn: "BinId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LocationComponents_Bins_BinId",
+                table: "LocationComponents");
+
+            migrationBuilder.DropIndex(
+                name: "IX_LocationComponents_BinId",
+                table: "LocationComponents");
+        }
+    }
+}
